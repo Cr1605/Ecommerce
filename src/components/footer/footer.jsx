bd77222afe092ffade242b9447fa8847f31b8a45
Cr1605 <allyaercc@gmail.com>
2023-05-14 19:39:31 +0700

    footer commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -13,15 +13,59 @@ const Footer = () => {
             <img src={Logo} alt="" />
             <span>Ecommerce</span>
         </div>
+
         <div className="block">
           <div className="detail">
             <span>Contact Us</span>
             <span className="pngLine">
-              {/* <LocationMarkerIcon className='icon'/> */}
+              <LocationMarkerIcon className='icon'/>
               <span>111 north avenue Orlando, FL 32801</span>
+            </span>
+            <span className="pngLine">
+              <PhoneIcon className='icon'/>
+              <a href="tel:352-306-4415">352-306-4415</a>
+            </span>
+            <span className="pngLine">
+              <InboxIcon className='icon'/>
+              <a href="mailto:thha.19it@gmail.com">thha.19it@gmail.com</a>
             </span>
           </div>
         </div>
+
+        <div className="block">
+          <div className="detail">
+            <span>Account</span>
+            <span className="pngLine">
+              <LoginIcon className='icon'/>
+              <span>Sign In</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="block">
+          <div className="detail">
+            <span>Company</span>
+            <span className="pngLine">
+              <UsersIcon className='icon'/>
+              <span>About us</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="block">
+          <div className="detail">
+            <span>Resources</span>
+            <span className="pngLine">
+              <LinkIcon className='icon'/>
+              <span>Safety Privacy & Terms</span>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="copyRight">
+        <span>Copyright ©2022 by Amazon, Inc.</span>
+        <span>All rights reserved.</span>
       </div>
     </div>
   )
