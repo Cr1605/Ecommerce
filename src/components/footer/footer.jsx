d3bb5336f115723b73d5bfb55719f12d4b61bf7a
Cr1605 <allyaercc@gmail.com>
2023-05-15 19:13:55 +0700

    footer commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css'
 import Logo from '../../assets/logo.png'
-import {InboxIcon, PhoneIcon, LocationMarkerIcon, LoginIcon, UsersIcon, LinkIcon} from '@heroicons/react/outline'
+import { InboxIcon, PhoneIcon, LocationMarkerIcon, LoginIcon, UsersIcon, LinkIcon } from '@heroicons/react/outline'
 
 
 const Footer = () => {
@@ -10,23 +10,23 @@ const Footer = () => {
       <hr />
       <div className="Footer">
         <div className="logo">
-            <img src={Logo} alt="" />
-            <span>Ecommerce</span>
+          <img src={Logo} alt="" />
+          <span>Ecommerce</span>
         </div>
 
         <div className="block">
           <div className="detail">
             <span>Contact Us</span>
             <span className="pngLine">
-              <LocationMarkerIcon className='icon'/>
+              <LocationMarkerIcon className='icon' />
               <span>111 north avenue Orlando, FL 32801</span>
             </span>
             <span className="pngLine">
-              <PhoneIcon className='icon'/>
+              <PhoneIcon className='icon' />
               <a href="tel:352-306-4415">352-306-4415</a>
             </span>
             <span className="pngLine">
-              <InboxIcon className='icon'/>
+              <InboxIcon className='icon' />
               <a href="mailto:thha.19it@gmail.com">thha.19it@gmail.com</a>
             </span>
           </div>
@@ -36,7 +36,7 @@ const Footer = () => {
           <div className="detail">
             <span>Account</span>
             <span className="pngLine">
-              <LoginIcon className='icon'/>
+              <LoginIcon className='icon' />
               <span>Sign In</span>
             </span>
           </div>
@@ -46,7 +46,7 @@ const Footer = () => {
           <div className="detail">
             <span>Company</span>
             <span className="pngLine">
-              <UsersIcon className='icon'/>
+              <UsersIcon className='icon' />
               <span>About us</span>
             </span>
           </div>
@@ -56,7 +56,7 @@ const Footer = () => {
           <div className="detail">
             <span>Resources</span>
             <span className="pngLine">
-              <LinkIcon className='icon'/>
+              <LinkIcon className='icon' />
               <span>Safety Privacy & Terms</span>
             </span>
           </div>
